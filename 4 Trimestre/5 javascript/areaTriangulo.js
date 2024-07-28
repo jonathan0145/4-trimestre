60cd687131451b6957f7ef3611f84798550b63f4
jonathan0145 <jonathan_bermeo014@hotmail.com>
2024-07-28 11:42:23 -0500

    javascript

---
+++ 4 Trimestre/5 javascript/areaTriangulo.js
@@ -0,0 +1,8 @@
+var base= parseInt( prompt("digite la base"));
+var altura=parseInt( prompt("digite la altura"));
+
+function areaTriangulo(base, altura){
+    return base*altura/2;
+}
+
+console.log(areaTriangulo(base, altura))
