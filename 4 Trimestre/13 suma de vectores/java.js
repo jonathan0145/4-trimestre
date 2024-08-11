8ac3cc739deca85ad895644f85f051ac73fbff91
jonathan0145 <jonathan_bermeo014@hotmail.com>
2024-08-11 13:42:57 -0500

    ultimo work del dia

--- 4 Trimestre/13 suma de vectores/java.js
+++ 4 Trimestre/13 suma de vectores/java.js
@@ -11,13 +11,16 @@ function sumarV(){
     v2 = v2.split(" ");
 
     if(v1.length > 5 || v2.length > 5){
-        pa.textContent = "digito mas de 5 numeros"; 
+        return pa.textContent = "digito mas de 5 numeros"; 
+    }
+    else if(v1.length < 5 || v2.length < 5){
+        return pa.textContent = "digito menos de 5 numeros"; 
     }
     else if(v1.length == 5 && v1.length == 5){
         for (let i = 0; i < v1.length && i < v2.length; i++) {
             sumaV[i] = parseInt(v1[i]) + parseInt(v2[i]);
         }
-        pa.textContent = sumaV;  
+        return pa.textContent = sumaV;  
     }
 
 }
