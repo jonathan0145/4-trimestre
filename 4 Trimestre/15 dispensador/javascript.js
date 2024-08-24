7a13c05f5cb7e168f9ba5740013b9890fc1580ad
jonathan0145 <jonathan_bermeo014@hotmail.com>
2024-08-24 11:41:04 -0500

    falta
    que muestre en pantalla el codigo con el producto que eligio

--- 4 Trimestre/15 dispensador/javascript.js
+++ 4 Trimestre/15 dispensador/javascript.js
@@ -56,6 +56,63 @@ class Producto {
     }
 }
 
+class codigos{
+    constructor(){
+        this.tablas = document.getElementById("tablas");
+
+        // Verifica si 'tabla' no es null
+        if (!this.tablas) {
+            console.error('Elemento con ID "tabla" no encontrado.');
+            return;
+        }
+
+        // Inicializa con digita codigo
+        this.total = "Digita codigo";
+
+        // Botones
+        const botonIds = [
+            "boton1", "boton2", "boton3", "boton4", "boton5",
+            "boton6", "boton7", "boton8", "boton9", 
+            "boton10"
+        ];
+
+        this.botones = botonIds.map(id => document.getElementById(id));
+
+        // Verifica si todos los botones están presentes y agrega el manejador de eventos
+        this.botones.forEach((boton) => {
+            if (!boton) {
+                console.error(`Elemento con ID "${boton.id}" no encontrado.`);
+            } else {
+                boton.addEventListener('click', this.mostrarValors.bind(this));
+            }
+        });
+
+         // Maneja el botón de comprar
+         const comprar = document.getElementById("botonComprar");
+         if (comprar) {
+             comprar.addEventListener('click', this.comprars.bind(this));
+         } else {
+             console.error('Elemento con ID "borrar" no encontrado.');
+        }
+    }
+
+    mostrarValors(Event) {
+        if (Event && Event.target) {
+            this.total = (Event.target.value);
+            // Muestra el total actualizado en el campo de entrada
+            this.tablas.value = this.total;
+        } else {
+            console.error('El evento es undefined o no tiene target.');
+        }
+    }
+
+    comprars() {
+        // Reinicia el total a 0 y actualiza el campo de entrada
+        this.total = "Digita codigo";
+        this.tablas.value = this.total;
+    }
+}
+
 class gaseosa{
     constructor(codigo,nombre,tamaño, envase,valor){
         this.codigo = codigo;
@@ -81,5 +138,15 @@ const cocav350 = new gaseosa(10,"coca cola", 350,"vidrio",3800);
 
 const gaseosas = [cocap250, cocap500,cocap1000, cocap1500, cocap2000, cocap2500, cocap3000,cocav200,cocav330, cocav350];
 
+// crear etiquetas li
+let productos = document.getElementById('productos')
+const lis =[];
+for (let i = 0; i < gaseosas.length; i++) {
+    lis[i] = document.createElement('li');
+    productos.appendChild(lis[i]);
+    lis[i].textContent = `codigo: ${gaseosas[i].codigo} ${gaseosas[i].nombre} ${gaseosas[i].tamaño}ml ${gaseosas[i].envase} valor: ${gaseosas[i].valor} `;
+}
+
 // Instancia de la clase
 const moneda = new Producto();
+const codi = new codigos();
